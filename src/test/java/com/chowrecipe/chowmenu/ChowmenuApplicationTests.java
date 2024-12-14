60cda8658aae60069c1e91f7c1c15194783bb265
Jiwen <given209@terpmail.umd.edu>
2024-12-14 11:53:51 -0800

    Initial commit

---
+++ src/test/java/com/chowrecipe/chowmenu/ChowmenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chowrecipe.chowmenu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChowmenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
