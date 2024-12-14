dfa3906172906fa52e5fc6a5aa4531c94268de6d
Jiwen <given209@terpmail.umd.edu>
2024-12-14 11:56:26 -0800

    test commit

--- src/main/java/com/chowrecipe/chowmenu/ChowmenuApplication.java
+++ src/main/java/com/chowrecipe/chowmenu/ChowmenuApplication.java
@@ -8,6 +8,7 @@ public class ChowmenuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ChowmenuApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
